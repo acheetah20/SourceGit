0ee9ab2fbc3c933d96045fde67a26d881e761029
dwang <dwang@WIN2016DEV06>
2020-03-26 15:19:05 -0400

    Add comment for application developement procedure

--- HelloUSA/HelloUSA/HelloUSA.cpp
+++ HelloUSA/HelloUSA/HelloUSA.cpp
@@ -2,11 +2,20 @@
 //                  Setup new project
 //                  Commit
 //                  Sync
-//                  Push
-
+//                  Push to create new repo
+//                  Create new branch if there is only master branch
+//                  Working on branch
+//                  Commit the change
+//                  Sync
+//                  push
+//                  review the code and merge the change
+//                  Pull the codeto your branch
+//                  end of change
+//
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello USA!\n";
+    std::cout << "We are united. No Coronavirus!\n";
 }
